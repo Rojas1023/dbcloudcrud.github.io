a5eb5ce52a1d25177cf628cb6b79f7916fe1460c
Jhoan Rojas <157655074+Rojas1023@users.noreply.github.com>
2025-02-27 09:15:31 -0500

    Update server.js

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const cors = require("cors");
 const path = require("path");
 
 const app = express();
-const port = process.env.PORT || 3000; // Railway asigna un puerto dinámico
+const port = process.env.PORT; // Railway asigna un puerto dinámico
 
 app.use(cors());
 app.use(express.json()); 
